5bfb3a0184289d5260d59f798ed21b41fb4d07de
Ana Prante <acprante@gmail.com>
2020-09-27 16:37:25 -0300

    Challenge done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+	const [repositories, setRepositories] = useState([]);
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+	useEffect(() => {
+	api.get('repositories').then(response => {
+		setRepositories(response.data);
+	})
+	}, []);
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+	async function handleAddRepository() {
+		const response = await api.post('repositories', {
+			title: `Título do repositório ${Date.now()}`,
+			url: 'https://github.com/acprante/teste',
+			techs: ['javascript', 'css']
+		});
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+		const repository = response.data;
+
+		setRepositories([...repositories, repository]);
+	}
+
+	async function handleRemoveRepository(id) {
+		const response = await api.delete('repositories/' + id);
+		
+		if(response.status === 204){
+			const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+			repositories.splice(repositoryIndex, 1);
+
+			setRepositories([...repositories]);
+		} else {
+			console.log('O repositório não pode ser excluído');
+		}
+	}
+
+	return (
+	<div>
+		<ul data-testid="repository-list">
+			{repositories.map(repository => { 
+				return (
+					<li key={repository.id} >
+						{repository.title}
+						<button onClick={() => handleRemoveRepository(repository.id)}>
+							Remover
+						</button>
+					</li>
+				);
+			}) }
+		</ul>
+
+		<button onClick={handleAddRepository}>Adicionar</button>
+	</div>
+	);
 }
 
 export default App;
